8d90fe08b111d6c3cb4be4687af454786d4a1a4d
Prof. Thiago Vieira de Aguiar <56936784+thiago-aguiar@users.noreply.github.com>
2022-10-18 13:11:00 +0000

    Aula 02: 18/10/22: Associando o JS ao HTML e um pouco de Variáveis.

---
+++ script.js
@@ -0,0 +1,27 @@
+// alert("Olá, são 20:19"); Isto está comentado.
+// Isto é um comentário
+
+/* Tudo o que está
+aqui dentro
+é um comentário; */
+
+// console.log("Olá.");
+// console.log("Tudo bem.");
+
+// Variáveis
+
+/* nome = "Thiago";
+idade = 90;
+console.log(nome);
+console.log(idade);
+
+idade = "Noventa";
+console.log(idade); */
+
+numeroUsuario = +prompt();
+
+numeroA = 10;
+numeroB = 20;
+
+somaAComB = numeroA + numeroB + numeroUsuario;
+console.log(somaAComB);
